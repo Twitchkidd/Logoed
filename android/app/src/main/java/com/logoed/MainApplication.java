db69f241dd875d3a221f339df48b9d0ef6927c73
Gareth Field <field.gareth@gmail.com>
2019-07-06 09:57:20 -0400

    react native 60!

--- android/app/src/main/java/com/logoed/MainApplication.java
+++ android/app/src/main/java/com/logoed/MainApplication.java
@@ -2,12 +2,16 @@ package com.logoed;
 
 import android.app.Application;
 
-import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.swmansion.rnscreens.RNScreensPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -20,11 +24,12 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
-      return packages;
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new LinearGradientPackage(),
+            new RNScreensPackage(),
+            new RNGestureHandlerPackage()
+      );
     }
 
     @Override
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
+import { AppRegistry } from "react-native";
+import App from "./src/router";
+import { name as appName } from "./app.json";
+import { useScreens } from "react-native-screens";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+useScreens();
 
 AppRegistry.registerComponent(appName, () => App);

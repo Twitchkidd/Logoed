5d363f44c7be53a60c27590c970fa451522458b2
Gareth Field <field.gareth@gmail.com>
2019-07-08 10:11:14 -0400

    save

--- src/screens/Logoing.js
+++ src/screens/Logoing.js
@@ -1,12 +1,72 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import React, { Component, createRef } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { RNCamera } from "react-native-camera";
 
 export default class Logoing extends Component {
+  constructor(props) {
+    super(props);
+    this.camera = createRef();
+  }
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={styles.container}>
+        <RNCamera
+          ref={this.camera}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: "Permission to use camera",
+            message: "We need your permission to use your camera",
+            buttonPositive: "Ok",
+            buttonNegative: "Cancel"
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: "Permission to use audio recording",
+            message: "We need your permission to use your audio",
+            buttonPositive: "Ok",
+            buttonNegative: "Cancel"
+          }}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
+        <View
+          style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}>
+          <TouchableOpacity onPress={this.takePicture} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "black"
+  },
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: "center",
+    margin: 20
+  }
+});

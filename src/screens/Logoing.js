a99c5a8f4a3d492629d04e4147aac524a7e85978
Gareth Field <field.gareth@gmail.com>
2019-07-23 13:34:43 -0400

    before trying linking and pod install

--- src/screens/Logoing.js
+++ src/screens/Logoing.js
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import styled from "styled-components/native";
 import { RNCamera } from "react-native-camera";
+import RNFS from "react-native-fs";
 import { eigengrau, lightOrange } from "../utilities";
 
 const PendingView = () => (
@@ -41,6 +42,30 @@ export default class Logoing extends PureComponent {
     const data = await camera.takePictureAsync(options);
     console.log(data.uri);
   };
+  componentDidMount() {
+    // get a list of files and directories in the main bundle
+    RNFS.readDir(RNFS.MainBundlePath) // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
+      .then(result => {
+        console.log("GOT RESULT", result);
+
+        // stat the first file
+        return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+      })
+      .then(statResult => {
+        if (statResult[0].isFile()) {
+          // if we have a file, read it
+          return RNFS.readFile(statResult[1], "utf8");
+        }
+        return "no file";
+      })
+      .then(contents => {
+        // log the file contents
+        console.log(contents);
+      })
+      .catch(err => {
+        console.log(err.message, err.code);
+      });
+  }
   render() {
     const { width } = Dimensions.get("window");
     return (

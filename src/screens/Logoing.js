ec10c4bbf91db17bb738e5c4239dac7f3e5e7753
Gareth Field <field.gareth@gmail.com>
2019-07-27 14:25:20 -0400

    start 2019-07-27

--- src/screens/Logoing.js
+++ src/screens/Logoing.js
@@ -4,13 +4,18 @@ import {
   View,
   Dimensions,
   Image,
-  ActivityIndicator
+  ActivityIndicator,
+  Modal,
+  Clipboard,
+  CameraRoll,
+  Linking
 } from "react-native";
 import styled from "styled-components/native";
 import { RNCamera } from "react-native-camera";
 import ViewShot from "react-native-view-shot";
 import { Button, P } from "../components";
 import Draggable from "react-native-draggable";
+import ImagePicker from "react-native-image-crop-picker";
 import { eigengrau, lightOrange, darkOrange, mostlyWhite } from "../utilities";
 
 const businesses = {
@@ -37,21 +42,23 @@ const ScreenWrapper = styled(View)`
   background-color: ${eigengrau};
 `;
 
+const Header = styled(View)`
+  flex: 0.2;
+  background-color: ${mostlyWhite};
+`;
+
 const CameraAndImageWrapper = styled(View)`
   flex: 1;
   width: ${props => props.screenWidth};
   z-index: 1;
 `;
-//height: ${props => props.screenWidth};
 
 const PendingView = styled(View)`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
-// TODO remove gesture to navigate back from logoing screen.
-// TODO ... probably welcome too
-// // position: relative;
+
 const Camera = styled(RNCamera)`
   width: ${props => props.screenWidth};
   height: ${props => props.screenWidth};
@@ -80,6 +87,12 @@ const ActionBar = styled(View)`
   z-index: 0;
 `;
 
+const CameraRollSelector = styled(TouchableOpacity)`
+  height: 41;
+  width: 41;
+  background-color: ${darkOrange};
+`;
+
 export default class Logoing extends Component {
   static navigationOptions = {
     header: null
@@ -96,15 +109,41 @@ export default class Logoing extends Component {
     snapShot: null,
     viewShot: null,
     x: 40,
-    y: 80
+    y: 80,
+    modalVisible: false,
+    caption: ""
   };
   retake = () => {
     this.setState({ pendingSnapShot: true });
   };
+  backToPreviousSnapShot = () => {
+    this.setState({ pendingSnapShot: false });
+  };
   takeSnapShot = async camera => {
     const options = { quality: 1.0, base64: true };
     const data = await camera.takePictureAsync(options);
-    this.setState({ pendingSnapShot: false, snapShot: data.uri });
+    this.setState({
+      pendingSnapShot: false,
+      snapShot: data.uri,
+      caption: `Ate at ${
+        businesses[id].handle
+      } and used @logoedapp to enter to win a raffle! Come on down!`
+    });
+  };
+  selectFromCameraRoll = () => {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 400
+    })
+      .then(image => {
+        this.setState({
+          snapShot: image,
+          caption: `Ate at ${
+            businesses[id].handle
+          } and used @logoedapp to enter to win a raffle! Come on down!`
+        });
+      })
+      .catch(err => console.log(err));
   };
   takeViewShot = () => {
     this.viewShot.current
@@ -124,6 +163,24 @@ export default class Logoing extends Component {
       y: y + Math.round(gestureState.dy)
     });
   };
+  popModal = () => {
+    const { caption } = this.state;
+    this.takeViewShot();
+    this.setState({ modalVisible: true });
+    Clipboard.setString(caption);
+  };
+  handleModalRequestClose = () => {
+    this.setState({ modalVisible: false });
+  };
+  share = () => {
+    const { viewShot } = this.state;
+    console.log("share");
+    CameraRoll.saveToCameraRoll(viewShot)
+      .then(() => console.log("camera roll save success!"))
+      .catch(err => console.log(err));
+    let instagramURL = `instagram://library?AssetPath=${viewShot}`;
+    Linking.openURL(instagramURL);
+  };
   render() {
     const { width } = Dimensions.get("window");
     const {
@@ -132,12 +189,15 @@ export default class Logoing extends Component {
       snapShot,
       viewShot,
       x,
-      y
+      y,
+      modalVisible,
+      caption
     } = this.state;
     const { id } = this.props.navigation.state.params;
     console.log(this.state);
     return (
       <ScreenWrapper>
+        <Header />
         <CameraAndImageWrapper screenWidth={width}>
           {pendingSnapShot ? (
             <Camera
@@ -191,16 +251,44 @@ export default class Logoing extends Component {
         <ActionBar screenWidth={width}>
           {pendingDraggedReady ? (
             <P>Tap and drag to place logo!</P>
-          ) : pendingSnapShot ? null : (
+          ) : pendingSnapShot && !snapShot ? (
+            <CameraRollSelector onPress={() => this.selectFromCameraRoll()} />
+          ) : pendingSnapShot && snapShot ? (
             <Fragment>
+              <CameraRollSelector onPress={() => this.selectFromCameraRoll()} />
+              <Button
+                onPress={() => this.backToPreviousSnapShot()}
+                title='Forward again'
+              />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <TopActionBar>
+                <P>{caption}</P>
+              </TopActionBar>
               <Button onPress={() => this.retake()} title='Back to camera!' />
               <Button
-                onPress={() => this.takeViewShot()}
+                onPress={() => this.popModal()}
                 title='Share to Instagram!'
               />
             </Fragment>
           )}
         </ActionBar>
+        <Modal
+          animationType='slide'
+          transparent={false}
+          visible={modalVisible}
+          onRequestClose={() => this.handleModalRequestClose()}>
+          <P>
+            Please allow to save the Logoed photo to the Camera Roll (PLEASE
+            INCLUDE CHECK FOR THIS STEP AND CONDITIONAL LOGIC)
+          </P>
+          <P>
+            Please share that photo to Instagram with the caption that's copied
+            to the clipboard to enter the raffle!
+          </P>
+          <Button onPress={() => this.share()} title='Share!' />
+        </Modal>
       </ScreenWrapper>
     );
   }

9de2a54299d12a2e4086cdcde0955e6f83aa785f
Gareth Field <field.gareth@gmail.com>
2020-04-07 12:31:15 -0400

    before adding vector-icons

--- src/screens/Logoing.js
+++ src/screens/Logoing.js
@@ -2,12 +2,13 @@ import React, { useState, useRef } from 'react';
 import styled from 'styled-components/native';
 import {
   ActivityIndicator,
+  Animated,
   ClipBoard,
   Dimensions,
   Image,
   Linking,
-  Modal,
   PixelRatio,
+  StyleSheet,
   Text,
   TouchableOpacity,
   View,
@@ -17,6 +18,7 @@ import { RNCamera } from 'react-native-camera';
 import CameraRoll from '@react-native-community/cameraroll';
 import ViewShot from 'react-native-view-shot';
 import Draggable from 'react-native-draggable';
+import Modal from 'react-native-modal';
 import {
   crayGray,
   eigengrau,
@@ -27,18 +29,74 @@ import {
 } from '../utils';
 import { restaurants } from '../data';
 
+const PendingCamera = () => (
+  <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+    <ActivityIndicator size="large" color={maroon} animating={true} />
+  </View>
+);
+
+const Shutter = props => (
+  <TouchableOpacity
+    style={{
+      flex: 0,
+      alignSelf: 'center',
+      height: 80,
+      width: 80,
+      backgroundColor: `${mostlyWhite}`,
+      borderColor: crayGray,
+      borderWidth: 2,
+      borderRadius: 40,
+      zIndex: 90,
+    }}
+    onPress={props.onPress}
+  />
+);
+
+const { width, height } = Dimensions.get('window');
+
 export const Logoing = ({ navigation, route }) => {
   const { business } = route.params;
   const restaurant = restaurants.filter(item => item.name === business)[0];
   const [x, setX] = useState(20);
   const [y, setY] = useState(80);
-  const [snapShot, setSnapShot] = useState(null);
+  const [modalin, setModalin] = useState(true);
   const [pendingDrag, setPendingDrag] = useState(true);
   const [pendingSnapShot, setPendingSnapShot] = useState(true);
+  const [snapShot, setSnapShot] = useState(null);
   const camera = useRef();
   const viewShot = useRef();
-  const { width, height } = Dimensions.get('window');
+  const infoBoxZoomAnim = useRef(new Animated.Value(0)).current;
+  const infoBoxFadeAnim = useRef(new Animated.Value(0)).current;
   const pixelRatio = PixelRatio.get();
+  const animIn = () => {
+    Animated.timing(infoBoxZoomAnim, {
+      toValue: 0,
+      duration: 500,
+    }).start();
+  };
+  const fadeIn = () => {
+    Animated.timing(infoBoxFadeAnim, {
+      toValue: 1,
+      duration: 500,
+    }).start();
+  };
+  const animOut = () => {
+    Animated.timing(infoBoxZoomAnim, {
+      toValue: 500,
+      duration: 500,
+    }).start();
+  };
+  const fadeOut = () => {
+    Animated.timing(infoBoxFadeAnim, {
+      toValue: 0,
+      duration: 500,
+    }).start();
+  };
   const setXY = (e, gestureState) => {
     setX(x + gestureState.dx / pixelRatio);
     setY(y + gestureState.dy / pixelRatio);
@@ -58,21 +116,21 @@ export const Logoing = ({ navigation, route }) => {
         CameraRoll.saveToCameraRoll(uri)
           .then(() => {
             console.log('camera roll save success!');
-            navigation.push("CopyCaption", { restaurant, viewShot: uri });
+            // navigation.push('CopyCaption', { restaurant, viewShot: uri });
           })
           .catch(err => console.log(err));
       })
       .catch(err => console.log(err));
   };
   return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          backgroundColor: eigengrau,
-        }}>
-        {pendingSnapShot ? (
-          <>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: eigengrau,
+      }}>
+      {pendingSnapShot ? (
+        <>
           <RNCamera
             ref={camera}
             type={RNCamera.Constants.Type.back}
@@ -91,21 +149,7 @@ export const Logoing = ({ navigation, route }) => {
               alignItems: 'center',
             }}>
             {({ camera, status }) => {
-              if (status !== 'READY')
-                return (
-                  <View
-                    style={{
-                      flex: 1,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}>
-                    <ActivityIndicator
-                      size="large"
-                      color={maroon}
-                      animating={true}
-                    />
-                  </View>
-                );
+              if (status !== 'READY') return <PendingCamera />;
               return (
                 <View
                   style={{
@@ -113,21 +157,9 @@ export const Logoing = ({ navigation, route }) => {
                     flexDirection: 'row',
                     justifyContent: 'center',
                   }}>
-                  <TouchableOpacity
-                    style={{
-                      flex: 0,
-                      alignSelf: 'center',
-                      height: 80,
-                      width: 80,
-                      backgroundColor: `${mostlyWhite}`,
-                      borderColor: crayGray,
-                      borderWidth: 2,
-                      borderRadius: 40,
-                      zIndex: 90,
-                    }}
-                    onPress={() => takeSnapShot(camera)}>
+                  <Shutter onPress={() => takeSnapShot(camera)}>
                     <View />
-                  </TouchableOpacity>
+                  </Shutter>
                 </View>
               );
             }}
@@ -141,23 +173,69 @@ export const Logoing = ({ navigation, route }) => {
             renderSize={width / 3}
           />
         </>
-        ) : (
-          <View
+      ) : (
+        <View
           style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <ViewShot ref={viewShot} options={{ format: 'jpg', quality: 1.0 }}>
-            <Image source={{ uri: snapShot }} style={{ width: width, height: height }} />
+            <Image
+              source={{ uri: snapShot }}
+              style={{ width: width, height: height }}
+            />
             <Draggable
               x={x * pixelRatio}
-              y={y * pixelRatio}
+              y={y * pixelRatio - 72}
               disabled
               z={3}
               onDragRelease={(e, gestureState) => setXY(e, gestureState)}
               imageSource={restaurant.logo}
               renderSize={width / 3}
             />
+            <Shutter onPress={() => setModalin(true)}>
+              <View />
+            </Shutter>
           </ViewShot>
-      </View>
-        )}
-      </View>
+          <Modal
+            isVisible={modalin}
+            onBackdropPress={() => setModalin(false)}
+            animationInTiming={500}
+            animationOutTiming={500}
+            backdropOpacity={0.6}
+            onModalWillShow={() => {
+              animIn();
+              fadeIn();
+            }}
+            onModalWillHide={() => {
+              animOut();
+              fadeOut();
+            }}
+            style={{ alignItems: 'center' }}>
+            <Animated.View
+              style={[
+                styles.infoBox,
+                {
+                  // marginBottom: infoBoxZoomAnim,
+                  marginLeft: infoBoxZoomAnim,
+                  opacity: infoBoxFadeAnim,
+                },
+              ]}>
+              <Text style={{ fontSize: 20, color: eigengrau }}>AYY!!</Text>
+            </Animated.View>
+          </Modal>
+        </View>
+      )}
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  infoBox: {
+    height: height / 3,
+    width: width - 80,
+    backgroundColor: mostlyWhite,
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: lightOrange,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
